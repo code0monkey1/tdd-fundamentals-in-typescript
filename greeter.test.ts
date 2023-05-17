7a6f97161e456d2c59d6edd0aabfd698cbe2c0ae
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-05-17 12:01:58 +0000

    17 May 2023, 17:31

---
+++ greeter.test.ts
@@ -0,0 +1,11 @@
+import { greeter } from "./greeter";
+
+describe("function exists", () => {
+
+  it("has a func", () => {
+
+      expect(greeter).toBeDefined();
+      expect(greeter).toBe(Function);
+  }) 
+  
+})
